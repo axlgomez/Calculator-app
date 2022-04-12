ace97df005e933638355ab0d7678a9db6e4a3bc3
Axel Gomez <axelgomez.js@gmail.com>
2022-04-12 23:57:22 +0200

    fix style, fix color sheme toggle

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
 
+// Adjust the color theme 
+// Have their initial theme preference checked and have any additional changes saved in the browser.
+
 const radioChange = () => {
     const checkes = document.querySelector('.theme__input:checked');
     const first = document.querySelector('#first');
@@ -9,38 +12,46 @@ const radioChange = () => {
         document.body.classList.add("first");
         document.body.classList.remove("third");
         document.body.classList.remove("second");       
-        localStorage.setItem('changeColor', 'first');
+        localStorage.setItem('colorScheme', 'first');
     }
     if(second == checkes){
         document.body.classList.add("second");
         document.body.classList.remove("third");
         document.body.classList.remove("first");
-        localStorage.setItem('changeColor', 'second');
+        localStorage.setItem('colorScheme', 'second');
     }
     if(third == checkes){
         document.body.classList.add("third");
         document.body.classList.remove("second");
         document.body.classList.remove("first");
-        localStorage.setItem('changeColor', 'third');
+        localStorage.setItem('colorScheme', 'third');
     }
 
 }
 
-
 let inputs = document.querySelectorAll('.theme__input');
 inputs.forEach(function(input){
     input.addEventListener('change', radioChange);
-    const color = localStorage.getItem('changeColor');
+    
+    const prefersColor = localStorage.getItem('colorScheme');
+    switch (prefersColor) {
+        case 'first':
+            document.body.classList.add("first");
+            first.setAttribute("checked", true);
+            break;
+        case 'second':
+            document.body.classList.add("second");
+            second.setAttribute("checked", true);
+            break;
+        case 'third':
+            document.body.classList.add("third");
+            third.setAttribute("checked", true);
+            break;
 
-    if(color === 'first'){
-        document.body.classList.add("first");
-    }
-    if(color === 'second'){
-        document.body.classList.add("second");
-    }
-    if(color === 'third'){
-        document.body.classList.add("third");
+        default:
+            first.setAttribute("checked", true);
+            break;
     }
 });
 
-
+//Perform mathematical operations like addition, subtraction, multiplication, and division
